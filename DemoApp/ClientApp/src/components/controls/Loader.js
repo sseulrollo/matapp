126f77e04330edbce137623f5faf387af5cd459b
veron08 <veron08@naver.com>
2019-10-30 14:17:38 +0900

    ma commit
    test

--- DemoApp/ClientApp/src/components/controls/Loader.js
+++ DemoApp/ClientApp/src/components/controls/Loader.js
@@ -1,14 +1,35 @@
-import React from 'react'
-import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-const LoaderExampleLoader = () => (
-  <Segment>
-    <Dimmer active>
-      <Loader />
-    </Dimmer>
+const useStyles = makeStyles(theme => ({
+  progress: {
+    margin: theme.spacing(2),
+  },
+}));
 
-    <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
-  </Segment>
-)
+export default function LoaderExampleLoader() {
+  const classes = useStyles();
 
-export default LoaderExampleLoader
+  return (
+    <div>
+      {/* <CircularProgress className={classes.progress} /> */}
+      <CircularProgress className={classes.progress} color="secondary" />
+    </div>
+  );
+}
+
+// import React from 'react'
+// import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
+
+// const LoaderExampleLoader = () => (
+//   <Segment>
+//     <Dimmer active>
+//       <Loader />
+//     </Dimmer>
+
+//     <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+//   </Segment>
+// )
+
+// export default LoaderExampleLoader
